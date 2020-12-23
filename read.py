2d79906e906df2adfd32d8859bb20e4f5783888b
hironeko <hironeko.sun@gmail.com>
2020-12-23 15:49:25 +0900

    done

--- read.py
+++ read.py
@@ -1,14 +1,43 @@
 import json
 import slackweb
 from os import getenv
+import datetime as dt
+import calendar
 
-data = json.load(open('./data/anime/202001.json', 'r'))
+today = dt.date.today()
+dayName = calendar.day_name[today.weekday()]
 
-webhookUrl = getenv('SLACK_WEBHOOK_URL')
+if today.month < 10:
+    month = f'0{today.month}'
+else:
+    month = today.month
 
-slack = slackweb.Slack(
-    url=webhookUrl)
+data = json.load(open(f'./data/anime/{today.year}{month}.json', 'r'))
+
+slack = slackweb.Slack(url=getenv('SLACK_WEBHOOK_URL'))
 
 for v in data:
-    print(v['title'])
-    slack.notify(text=v['title'])
+    if v['day_of_week'] == dayName:
+        attachments = [
+           {
+               "fallback": 'アニメの放送時間のご案内',
+               "pretext": '本日放送のアニメ',
+               "fields": [
+                   {
+                       "title": 'タイトル',
+                       "value": v['title'],
+                   },
+                   {
+                       "title": '放送時間',
+                       "value": v['publish_at'],
+                       "short": "true"
+                   },
+                   {
+                       "title": 'チャンネル',
+                       "value": v['channel'],
+                       "short": "true"
+                   }
+               ]
+           } 
+        ]
+        slack.notify(attachments=attachments)
