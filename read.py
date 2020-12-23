08771b72eef48df531b1697d6c6d2bb4cf0568df
hironeko <hironeko.sun@gmail.com>
2020-12-23 13:18:36 +0900

    first commit

---
+++ read.py
@@ -0,0 +1,14 @@
+import json
+import slackweb
+from os import getenv
+
+data = json.load(open('./data/anime/202001.json', 'r'))
+
+webhookUrl = getenv('SLACk_WEBHOOK_URL')
+
+slack = slackweb.Slack(
+    url=webhookUrl)
+
+for v in data:
+    print(v['title'])
+    slack.notify(text=v['title'])
